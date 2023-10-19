da3d08d079d2fc6e3fdf112bf692818402d81840
Dayvid Emerson <dayvidemerson96@gmail.com>
2023-10-19 14:15:42 -0300

    Create gem config

---
+++ lib/brasilapi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "brasilapi/version"
+
+module Brasilapi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/brasilapi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Brasilapi do
+  it "has a version number" do
+    expect(Brasilapi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
