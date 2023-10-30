e7754cbfdf4bb6c3cd9e008924856be3726701b3
Dayvid Emerson <dayvidemerson96@gmail.com>
2023-10-30 12:12:37 -0300

    feat: add service to get brasilian national holidays by year

--- lib/brasil_api.rb
+++ lib/brasil_api.rb
@@ -5,6 +5,7 @@ require_relative "brasil_api/base"
 require_relative "brasil_api/address"
 require_relative "brasil_api/bank"
 require_relative "brasil_api/company"
+require_relative "brasil_api/holiday"
 
 module BrasilAPI
   class Error < StandardError; end
