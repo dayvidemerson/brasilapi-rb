6c5f7fbee2f56924fd85b9323dfe6d5517c9dd64
Dayvid Emerson <dayvidemerson96@gmail.com>
2024-02-25 18:52:29 -0300

    test: check get state info by code

--- spec/brasil_api/ibge_spec.rb
+++ spec/brasil_api/ibge_spec.rb
@@ -17,6 +17,31 @@ RSpec.describe BrasilAPI::IBGE do
     end
   end
 
+  describe ".find_state_by_code" do
+    let(:piaui_code) { 22 }
+    let(:rio_de_janeiro_code) { "RJ" }
+    let(:invalid_code) { 0 }
+    let(:piaui_info) { JSON.parse(File.read("spec/fixtures/ibge/piaui_info.json")) }
+
+    it "does not raise error" do
+      VCR.use_cassette("ibge/find_state_by_code/rio_de_janeiro") do
+        expect { described_class.find_state_by_code(rio_de_janeiro_code) }.not_to raise_error
+      end
+    end
+
+    it "returns piaui info from ibge" do
+      VCR.use_cassette("ibge/find_state_by_code/piaui") do
+        expect(described_class.find_state_by_code(piaui_code)).to eq(piaui_info)
+      end
+    end
+
+    it "returns error not found" do
+      VCR.use_cassette("ibge/find_state_by_code/not_found") do
+        expect { described_class.find_state_by_code(invalid_code) }.to raise_error(BrasilAPI::NotFound)
+      end
+    end
+  end
+
   describe ".cities_by_state" do
     let(:piaui_abbr) { "PI" }
     let(:sao_paulo_abbr) { "SP" }
