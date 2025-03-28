0778f18f3a9012b42bdac3e88872275d01f8ed21
r3tr0boy <r3tr0boy1234@proton.me>
2025-03-28 11:04:12 -0300

    Commits conflitantes resolvidos

--- lib/brasil_api/taxas.rb
+++ lib/brasil_api/taxas.rb
@@ -3,16 +3,11 @@
 module BrasilAPI
   class Taxas < Base
     class << self
-<<<<<<< HEAD
-      def rates(name=nil)
-        return get("/taxas/v1") if name.nil?
-=======
       def all
         get("/taxas/v1")
       end
 
       def find_by_name(name)
->>>>>>> 8333a3c (ajustes na nomeclatura)
         get("/taxas/v1/#{name}")
       end
     end
